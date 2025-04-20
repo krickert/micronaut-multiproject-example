f3d423b55336ffc2ec3d452ebb19510959caaee8
Kristian Rickert <krickert@gmail.com>
2025-04-20 19:38:34 -0400

    produces asciidoc with code.  Going to fix mermaid next and make sure this happens by default

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,29 @@
 // build.gradle.kts (Root project)
 plugins {
+    id("org.asciidoctor.jvm.convert") version "4.0.4"
     base
+}
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.asciidoctor:asciidoctor-gradle-jvm:4.0.4")
+    }
+}
+
+tasks.named<org.asciidoctor.gradle.jvm.AsciidoctorTask>("asciidoctor") {
+    setSourceDir(file("docs"))
+    setOutputDir(file("./build/docs"))
+    baseDirFollowsSourceFile()
+    // Any attributes needed by your documentation
+    attributes(
+        mapOf(
+            "source-highlighter" to "highlight.js",
+            "toc" to "left",
+            "toclevels" to "3"
+        )
+    )
 }
 
 group = "com.yourcompany.pipeline"
