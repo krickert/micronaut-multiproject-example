4ad7b979a99965844f7737d893f882ae57dd52d5
krickert <krickert@gmail.com>
2025-05-22 17:01:28 -0400

    fun experiement to have grpc client -> java grpc server -> python code and back

--- yappy-modules/semantic-chunker/src/main/python/semantic_chunker/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Semantic Chunker module for the YAPPY platform.
-
-This module provides a gRPC service that implements the PipeStepProcessor interface
-to perform semantic chunking on text data.
-"""
-
-__version__ = "1.0.0"
--- yappy-modules/semantic-chunker/src/test/python/semantic_chunker/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Test package for the Semantic Chunker module.
-"""
