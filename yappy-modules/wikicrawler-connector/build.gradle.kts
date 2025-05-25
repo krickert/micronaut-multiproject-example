88284927311b0672f14761e97fbb3d061856710a
Kristian Rickert <krickert@gmail.com>
2025-05-25 18:04:12 -0400

    improvement

--- yappy-modules/wikicrawler-connector/build.gradle.kts
+++ yappy-modules/wikicrawler-connector/build.gradle.kts
@@ -1,10 +1,7 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import io.micronaut.testresources.buildtools.KnownModules
-
 plugins {
-    id("io.micronaut.minimal.application") version libs.versions.micronautPlugins.get()
-    id("com.gradleup.shadow") version "8.3.6" // Not in libs.versions.toml
-    `java-library`
+    id("io.micronaut.minimal.application") version "4.5.3"
+    id("io.micronaut.test-resources") version "4.5.3"
+    id("com.gradleup.shadow") version "8.3.6"
 }
 
 group = "com.krickert.yappy.modules.wikicrawlerconnector"
@@ -15,26 +12,15 @@ repositories {
 }
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.toVersion("21")
+    targetCompatibility = JavaVersion.toVersion("21")
 }
 
 micronaut {
-    runtime.set(io.micronaut.gradle.MicronautRuntime.NONE)
     testRuntime("junit5")
     processing {
         incremental(true)
         annotations("com.krickert.yappy.wikicrawler.*")
-    }
-    testResources {
-        enabled.set(true)
-        inferClasspath.set(true)
-        clientTimeout.set(60)
-        sharedServer.set(true)
-        knownModules.add(KnownModules.KAFKA)
     }
 }
 
@@ -42,13 +28,11 @@ dependencies {
     // Micronaut - using mn notation which should resolve via version catalog
     annotationProcessor(mn.micronaut.inject.java) // maps to libs.micronaut.inject.java
     annotationProcessor(mn.micronaut.serde.processor)
-    annotationProcessor(libs.micronaut.openapi.visitor) // For OpenAPI spec generation
     implementation(mn.micronaut.inject)
     implementation(mn.micronaut.serde.jackson)
     implementation(mn.micronaut.http.client)
     implementation(mn.micronaut.management)
     implementation(mn.micronaut.grpc.runtime)
-    implementation(libs.micronaut.openapi.ui) // For Swagger UI
 
     // Yappy Models (Protobufs)
     implementation(project(":yappy-models:protobuf-models"))
@@ -81,20 +65,18 @@ dependencies {
     testResourcesImplementation(project(":yappy-test-resources:apache-kafka-test-resource"))
     testImplementation(project(":yappy-test-resources:apicurio-test-resource"))
     testResourcesImplementation(project(":yappy-test-resources:apicurio-test-resource"))
+    implementation(mn.micronaut.reactor.http)
 }
 
-tasks.withType<ShadowJar> {
-    archiveClassifier.set("shaded")
-
-    relocate("info.bliki", "com.krickert.shaded.bliki")
-    relocate("org.xml.sax", "com.krickert.shaded.org.xml.sax")
-
-    exclude("META-INF/LICENSE*")
-    exclude("META-INF/NOTICE*")
-    exclude("META-INF/DEPENDENCIES*")
+application {
+    mainClass = "com.krickert.yappy.wikicrawler.WikiCrawlerApplication"
 }
 
-// Ensure compilation happens before shadowJar if shadowJar is a primary artifact
-// tasks.named("build") {
-// dependsOn(tasks.shadowJar)
-// }
+sourceSets {
+    main {
+        java {
+            srcDirs("build/generated/source/proto/main/grpc")
+            srcDirs("build/generated/source/proto/main/java")
+        }
+    }
+}
