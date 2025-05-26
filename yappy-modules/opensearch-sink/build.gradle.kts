54ff073b0c3552a7740f228be18043bac483f5d8
Kristian Rickert <krickert@gmail.com>
2025-05-26 10:12:17 -0400

    turned off shared server.. figured it out - the FIRST test resource needs to load everything for it to work for ALL of them

--- yappy-modules/opensearch-sink/build.gradle.kts
+++ yappy-modules/opensearch-sink/build.gradle.kts
@@ -108,7 +108,7 @@ micronaut {
         enabled.set(true)
         inferClasspath.set(true)
         clientTimeout.set(60)
-        sharedServer.set(true)
+        sharedServer.set(false)
         debugServer.set(false)
     }
 }
